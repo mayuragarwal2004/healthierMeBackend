884695b252ae280c0376b469ce34f0868ff880e7
Mayur Agarwal <mayur.agarwal22@vit.edu>
2023-10-08 11:56:36 +0530

    merged events and tasks

--- models/events/event.queries.sql.js
+++ models/events/event.queries.sql.js
@@ -82,52 +82,6 @@ const createCEvent = async(cId, eventArr)=>{
 
 }
 
-module.exports = {createCEvent, validateEvenconst Event = require("./event.model");
-
-const readEvent = async (cId) => {
-  let eventCObj = await Event.findOne({ cId: cId }).catch((err) => {
-    console.log(err);
-    return -1;
-  });
-
-  return eventCObj.event;
-};
-
-const validateEvent = async (eItem) => {
-  if (
-    !eItem.eId ||
-    !eItem.eName ||
-    !eItem.eDesc ||
-    !eItem.eFreq ||
-    !eItem.eStart ||
-    !eItem.eEnd
-  ) {
-    return false;
-  }
-
-  if (
-    await Event.findOne({ "event.eId": eItem.eId }).catch((err) => {
-      console.log(err);
-      return false;
-    })
-  ) {
-    return false;
-  }
-  return true;
-};
-
-const createCEvent = async (cId, eventArr) => {
-  const createCEventMain = await Event.create({
-    cId: cId,
-    event: eventArr,
-  }).catch((err) => {
-    console.log(err);
-    return -1;
-  });
-
-  return createCEventMain;
-};
-
 const listEvents = async (uID, communityId, challengeId) => {
   if (!uID || !communityId || !challengeId) {
     return 0;
@@ -161,4 +115,4 @@ const listEvents = async (uID, communityId, challengeId) => {
   }
 };
 
-module.exports = { createCEvent, validateEvent, readEvent, listEvents };
+module.exports = { createCEvent, validateEvent, listEvents, readEventByCid };
--- models/tasks/task.queries.sql.js
+++ models/tasks/task.queries.sql.js
@@ -81,49 +81,6 @@ const createCTask = async (cId, taskArr) => {
 
 };
 
-module.exports = { createCTask, validateTask, readTaskByTid, readTaskByCid };
-
-const Task = require("./task.model");
-
-const readTask = async(cId) =>{
-  let taskCObj = await Task.findOne({ "cId": cId }).catch((err) => {
-    console.log(err);
-    return -1;
-  })
-
-  return taskCObj.task
-}
-
-const validateTask = async(tItem) =>{
-    if(!tItem.tId || !tItem.tName || !tItem.tDesc || !tItem.tQuant || !tItem.tUnit || !tItem.tPeriodUnit || !tItem.tTC || !tItem.tStart || !tItem.tEnd){
-        return false
-    }
-
-    if(await Task.findOne({"task.tId": tItem.tId }).catch((err) => {
-      console.log(err);
-      return false;
-    })
-  ){
-        return false
-    }
-    return true
-
-}
-
-const createCTask = async (cId, taskArr) => {
-
-  const createCTaskMain = await Task.create({
-    cId: cId,
-    task: taskArr,
-  }).catch((err) => {
-    console.log(err);
-    return -1;
-  });
-
-  return createCTaskMain;
-
-};
-
 const listTasks = async (uID, communityId, challengeId) => {
   if (!uID || !communityId || !challengeId) {
     return 0;
@@ -157,4 +114,4 @@ const listTasks = async (uID, communityId, challengeId) => {
   }
 };
 
-module.exports = { createCTask, validateTask, readTask, listTasks };
+module.exports = { createCTask, validateTask, readTask, listTasks, readTaskByTid, readTaskByCid };
