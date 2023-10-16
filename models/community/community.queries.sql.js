7a8bacdf8ff72e1b750f55943bdf86b0e95d57a4
Mayur Agarwal <mayur.agarwal22@vit.edu>
2023-10-17 02:20:14 +0530

    community-query-update

--- models/community/community.queries.sql.js
+++ models/community/community.queries.sql.js
@@ -2,28 +2,25 @@ const { query } = require("express");
 const con = require("../../connection.sql");
 var mysql = require("mysql");
 
-const readUserCommunity = async(communityId)=>{
+const readUserCommunity = async (communityId) => {
   //query to read all users from a community
-  queryResult = await new Promise((resolve, reject)=>{
+  queryResult = await new Promise((resolve, reject) => {
     con.query(
       `SELECT CommunityUserMapping.user_id, CommunityUserMapping.role, CommunityUserMapping.community_id, User.first_name, User.last_name, User.dob  FROM HealthierMe.CommunityUserMapping 
       INNER JOIN HealthierMe.User on HealthierMe.CommunityUserMapping.user_id = HealthierMe.User.user_id
-      WHERE community_id='${communityId}';`
-      ,
+      WHERE community_id='${communityId}';`,
       function (err, result, fields) {
         if (err) {
-          reject(err) 
-          }
-        else resolve(result);
+          reject(err);
+        } else resolve(result);
       }
-    )
-  }).catch((err)=>{
-    console.log(err)
+    );
+  }).catch((err) => {
+    console.log(err);
     return -1;
-  }
-    )
-    return queryResult;
-}
+  });
+  return queryResult;
+};
 const readCommunity = async (cId) => {
   queryResult = await new Promise((resolve, reject) => {
     con.query(
@@ -40,54 +37,53 @@ const readCommunity = async (cId) => {
   });
 
   return queryResult[0];
-}
+};
 
 const readCommunityByJoinCode = async (joinCode) => {
-
-    const queryResult = await new Promise((resolve, reject) => {
-      con.query(
-        `SELECT community_id, community_name
+  const queryResult = await new Promise((resolve, reject) => {
+    con.query(
+      `SELECT community_id, community_name
         FROM HealthierMe.Community
         WHERE community_join_code = '${joinCode}';`,
-        (err, result, fields) => {
-          if (err) {
-            reject(err);
-            return;
-          }
-
-          resolve(result);
+      (err, result, fields) => {
+        if (err) {
+          reject(err);
+          return;
         }
-      );
-    }).catch((err) => {
-      console.log(err);
-      return -1;
-    });
-    return queryResult[0];
-}
 
+        resolve(result);
+      }
+    );
+  }).catch((err) => {
+    console.log(err);
+    return -1;
+  });
+  return queryResult[0];
+};
 
 const validateCommunity = async (cItem) => {
-  if (!cItem.communityId || 
-      !cItem.communityName || 
-      !cItem.locality ||
-      !cItem.pincode || 
-      !cItem.city || 
-      !cItem.state || 
-      !cItem.userId) {
+  if (
+    !cItem.communityId ||
+    !cItem.communityName ||
+    !cItem.locality ||
+    !cItem.pincode ||
+    !cItem.city ||
+    !cItem.state ||
+    !cItem.userId
+  ) {
     return 0;
   }
 
-  commObj = await readCommunity(cItem.communityId)
+  commObj = await readCommunity(cItem.communityId);
 
   if (commObj == -1) {
     return -1;
   }
-   if(commObj){
+  if (commObj) {
     return -2;
-   }
+  }
   return 1;
-}
-
+};
 
 //use these parameters to create a community community_id VARCHAR(255) PRIMARY KEY,
 // community_join_code VARCHAR(6) NOT NULL,
@@ -97,36 +93,46 @@ const validateCommunity = async (cItem) => {
 // city VARCHAR(255) NOT NULL,
 // state VARCHAR(255) NOT NULL,
 // created_by_user_id VARCHAR(255) NOT NULL,
-// created_datetime DATETIME NOT NULL, 
-// members_count INT DEFAULT 1,                    
-// description TEXT, 
+// created_datetime DATETIME NOT NULL,
+// members_count INT DEFAULT 1,
+// description TEXT,
 // access ENUM('Open', 'Admin_control', 'Predefined'),
-  
+
 const createCommunity = async (cItem) => {
-const { communityId, joinCode, communityName, locality, pincode, city, state, userId, desc } = cItem;
-let sql_desc = desc ? ", description" : "";
-let main_desc = desc ? `, '${desc}'` : "";
-queryResult = await new Promise((resolve, reject) => {
-  //write query to create community
-  con.query(  
-    `INSERT INTO HealthierMe.Community  
+  const {
+    communityId,
+    joinCode,
+    communityName,
+    locality,
+    pincode,
+    city,
+    state,
+    userId,
+    desc,
+  } = cItem;
+  let sql_desc = desc ? ", description" : "";
+  let main_desc = desc ? `, '${desc}'` : "";
+  queryResult = await new Promise((resolve, reject) => {
+    //write query to create community
+    con.query(
+      `INSERT INTO HealthierMe.Community  
     (community_id, community_join_code, community_name, locality, pincode, city, state, created_by_user_id ${sql_desc})
     VALUES
     ('${communityId}', '${joinCode}', '${communityName}', '${locality}', '${pincode}', '${city}', '${state}', '${userId}' ${main_desc});`,
-    function (err, result, fields) {
-      if (err) {
-        reject(err);
-      } else resolve(result);
-    }
-  );
-}).catch((err) => {
-  console.log(err);
-  return -1;
-});
-return cItem;
-}
+      function (err, result, fields) {
+        if (err) {
+          reject(err);
+        } else resolve(result);
+      }
+    );
+  }).catch((err) => {
+    console.log(err);
+    return -1;
+  });
+  return cItem;
+};
 
-const joinCommunity = async (communityId, userId, role ) => {
+const joinCommunity = async (communityId, userId, role) => {
   queryResult = await new Promise((resolve, reject) => {
     con.query(
       `INSERT INTO HealthierMe.CommunityUserMapping
@@ -144,7 +150,7 @@ const joinCommunity = async (communityId, userId, role ) => {
     return -1;
   });
   return queryResult;
-}
+};
 
 const updateMemberCount = async (cId) => {
   const queryResult = await new Promise((resolve, reject) => {
@@ -161,13 +167,12 @@ const updateMemberCount = async (cId) => {
         resolve(result);
       }
     );
-  }
-  ).catch((err) => {
+  }).catch((err) => {
     console.log(err);
     return -1;
   });
   return queryResult;
-}
+};
 
 const allCommunities = async (uID) => {
   if (!uID) {
@@ -177,7 +182,7 @@ const allCommunities = async (uID) => {
   try {
     const queryResult = await new Promise((resolve, reject) => {
       con.query(
-        `SELECT C.community_id, C.community_name, C.description, C.last_updated, CU.role
+        `SELECT C.community_id, C.community_name, C.description, C.last_updated, CU.role, C.locality, C.pincode, C.city, C.state, C.access
         FROM HealthierMe.Community AS C
         INNER JOIN HealthierMe.CommunityUserMapping AS CU
         ON C.community_id = CU.community_id
@@ -233,4 +238,14 @@ const verifyUserCommunity = async (uID, communityId) => {
   }
 };
 
-module.exports = { readUserCommunity, readCommunityByJoinCode, allCommunities, verifyUserCommunity, readCommunity, validateCommunity, createCommunity, joinCommunity, updateMemberCount };
+module.exports = {
+  readUserCommunity,
+  readCommunityByJoinCode,
+  allCommunities,
+  verifyUserCommunity,
+  readCommunity,
+  validateCommunity,
+  createCommunity,
+  joinCommunity,
+  updateMemberCount,
+};
